56c0740f1939c5a1e7fc15cd4edcaebbe372b2df
rio123o <113337983+rio123o@users.noreply.github.com>
2024-09-30 22:04:57 +0900

    プレイヤーがゴールした時に、ゴール画面が出るようにした

--- MIZU/Assets/Scripts/goalDecisionScript.cs
+++ MIZU/Assets/Scripts/goalDecisionScript.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class goalDecisionScript : MonoBehaviour
 {
+    public bool isColliding = false;
     private void OnTriggerEnter(Collider other)
     {
         //  tag‚ªgoal‚¾‚Á‚½Žž
-        if (other.CompareTag("goal"))
-        { 
-        Debug.Log("goal");
-        Destroy(gameObject);
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("goal");
+            isColliding = true;
+            Destroy(other.gameObject);
         }
     }
 
