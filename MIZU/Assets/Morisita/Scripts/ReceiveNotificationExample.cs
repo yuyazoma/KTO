98d0b7229e2181bf0d2c9d26e627f57aa9e4bcf0
suijumoku <annryuuki@gmail.com>
2024-10-01 11:07:55 +0900

    プレイヤーのリスポーンのプログラミング中

---
+++ MIZU/Assets/Morisita/Scripts/ReceiveNotificationExample.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ReceiveNotificationExample : MonoBehaviour
+{
+    // プレイヤー入室時に受け取る通知
+    public void OnPlayerJoined(PlayerInput playerInput)
+    {
+        print($"プレイヤー#{playerInput.user.index}が入室！");
+    }
+
+    // プレイヤー退室時に受け取る通知
+    public void OnPlayerLeft(PlayerInput playerInput)
+    {
+        print($"プレイヤー#{playerInput.user.index}が退室！");
+    }
+}
