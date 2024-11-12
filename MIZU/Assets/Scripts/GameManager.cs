63f1a3c74604e89e247766fd8fd52cfce020dac8
rio123o <113337983+rio123o@users.noreply.github.com>
2024-11-12 12:17:51 +0900

    goalSceneの作成と遷移

--- MIZU/Assets/Scripts/GameManager.cs
+++ MIZU/Assets/Scripts/GameManager.cs
@@ -19,4 +19,9 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene("stage gimic");
     }
+
+    public void RetrunTitle()
+    {
+        SceneManager.LoadScene("titleScene");
+    }
 }
