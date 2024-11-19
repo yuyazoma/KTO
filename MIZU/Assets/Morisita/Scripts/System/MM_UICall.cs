a3483670b3cf41786309d7906933774d82b834ee
suijumoku <annryuuki@gmail.com>
2024-11-19 14:41:48 +0900

    プレイヤーが固体状態の時のアニメーションをできるようにしました

---
+++ MIZU/Assets/Morisita/Scripts/System/MM_UICall.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MM_UICall : MonoBehaviour
+{
+    [SerializeField]
+    private Transform UIRoot;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
